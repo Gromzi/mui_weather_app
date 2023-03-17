fe3b1402bd7c3519599d2da23e4945b174e158b9
Gromzi <mamgier@gmail.com>
2023-03-17 20:59:38 +0100

    Initial config

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./styles/main.scss"
+import { Typography } from "@mui/material"
+
+function App() {
+  return (
+    <div>
+      <Typography variant="h4" align="center">
+        <p>MUI Weather App</p>
+      </Typography>
+    </div>
+  )
+}
+
+export default App
